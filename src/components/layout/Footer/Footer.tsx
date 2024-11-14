0a13a924b53daa50107ebc1c5c7bab37c61b84fe
Chris Coutts <chris.coutts@sunderlandsoftwarecity.com>
2024-11-14 18:23:29 +0000

    Add content to footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { TransitionLink } from "../navigation/TransitionLink/TransitionLink";
+import { NavLink } from "../navigation/NavLink/NavLink";
 
 interface FooterProps {}
 const Footer = () => {
@@ -7,41 +9,62 @@ const Footer = () => {
       className="relative h-[300px]"
       style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}>
       <div className="fixed bottom-0 h-[300px] w-full">
-        <div className="flex h-full w-full flex-col justify-between bg-[#3C4254] px-4 py-8 text-[#e4e8ed]  md:px-12 lg:px-32">
+        <div className="flex h-full w-full flex-col justify-between bg-[#BFBCDC] px-4 py-8 md:px-12 lg:px-32">
           <nav className="flex w-full justify-between">
-            <div className="flex w-full flex-col justify-between">
-              <h3 className="b-bottom uppercase text-[#F17A14]">Navigation</h3>
-              <ul className="gap-4">
-                <li>nice</li>
-              </ul>
-            </div>
-            <section className="flex w-full flex-col justify-between text-right">
-              <h3 className="b-bottom uppercase text-[#F17A14]">Contact details</h3>
-              <ul className="gap-4">
-                <li className="text-sm md:text-base">
-                  <Link href="mailto:Info@ewmbespokeinteriors.co.uk">
-                    <p>info@ewmbespokeinteriors.co.uk</p>
-                  </Link>
+            <section className="flex w-full flex-col justify-between gap-4">
+              <h3 className="b-bottom uppercase">Sitemap</h3>
+              <ul className="gap-2 flex flex-col">
+                <li>
+                  <TransitionLink href="/" underline>
+                    Home
+                  </TransitionLink>
                 </li>
-                <li>07515829261</li>
-                <li>07734387217</li>
+                <li>
+                  <TransitionLink href="/about" underline>
+                    About
+                  </TransitionLink>
+                </li>
+                <li>
+                  <TransitionLink href="/work" underline>
+                    Work
+                  </TransitionLink>
+                </li>
+                <li>
+                  <TransitionLink href="/contact" underline>
+                    Contact
+                  </TransitionLink>
+                </li>
               </ul>
             </section>
+            <aside className="flex w-full flex-col justify-between text-right">
+              <h3 className="b-bottom uppercase">Let's connect</h3>
+              <ul className="gap-4">
+                <li>
+                  <NavLink href="mailto:Info@ewmbespokeinteriors.co.uk">
+                    chris.dcoutts@gmail.com
+                  </NavLink>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.linkedin.com/in/chris-coutts/"
+                    target="_blank">
+                    LinkedIn
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://github.com/ccoutts2/" target="_blank">
+                    GitHub
+                  </Link>
+                </li>
+              </ul>
+            </aside>
           </nav>
-          <div className="flex items-end justify-between">
-            <Link href="/">
-              <div className="h-[5rem] w-[10rem]">
-                <img
-                  className="h-full w-full rounded-md object-cover"
-                  src="/assets/logos/ewm.jpg"
-                  alt="EWM company logo"
-                />
-              </div>
-            </Link>
-            <ul className="flex gap-4">
-              <li>hi</li>
-            </ul>
-          </div>
+          <dl className="flex items-end justify-between">
+            <dt className="Website Information"></dt>
+            <dd>
+              <small>Website designed and built by Chris Coutts</small>
+            </dd>
+          </dl>
         </div>
       </div>
     </footer>
