915e6ca7a33345d8cf9c2989dc3e5d44c87d2d38
Chris Coutts <ccoutts@mmgy.com>
2025-08-28 16:41:15 +0100

    Comment out unused code

--- src/components/MobileContactForm/MobileContactForm.tsx
+++ src/components/MobileContactForm/MobileContactForm.tsx
@@ -1,30 +1,30 @@
-"use client";
-import styles from "./MobileContactForm.module.css";
-import { useState } from "react";
-import { Button } from "../ui/buttons/Button";
-import ContactForm from "../ContactForm/ContactForm";
-import classNames from "classnames";
+// "use client";
+// import styles from "./MobileContactForm.module.css";
+// import { useState } from "react";
+// import { Button } from "../ui/buttons/Button";
+// import ContactForm from "../ContactForm/ContactForm";
+// import classNames from "classnames";
 
-const MobileContactForm = () => {
-  const [isFormOpen, setIsFormOpen] = useState(false);
+// const MobileContactForm = () => {
+//   const [isFormOpen, setIsFormOpen] = useState(false);
 
-  const onClick = () => {
-    setIsFormOpen(!isFormOpen);
-  };
+//   const onClick = () => {
+//     setIsFormOpen(!isFormOpen);
+//   };
 
-  return (
-    <>
-      <div className="p-4 md:hidden">
-        <Button onClick={onClick}>Contact Form</Button>
-      </div>
+//   return (
+//     <>
+//       <div className="p-4 md:hidden">
+//         <Button onClick={onClick}>Contact Form</Button>
+//       </div>
 
-      <div
-        {...(isFormOpen && { "data-is-open": true })}
-        className={classNames(styles.FormOverlay, "relative")}>
-        {isFormOpen && <ContactForm onClick={onClick} />}
-      </div>
-    </>
-  );
-};
+//       <div
+//         {...(isFormOpen && { "data-is-open": true })}
+//         className={classNames(styles.FormOverlay, "relative")}>
+//         {isFormOpen && <ContactForm onClick={onClick} />}
+//       </div>
+//     </>
+//   );
+// };
 
-export default MobileContactForm;
+// export default MobileContactForm;
